53f92b7b9783a5cb055ad6dfae19a09265e24c1a
Yashwanth M <139760313+yashwanthm998@users.noreply.github.com>
2024-10-09 11:48:06 +0530

    Update Resume.jsx

--- src/pages/Resume.jsx
+++ src/pages/Resume.jsx
@@ -11,7 +11,7 @@ import "react-pdf/dist/esm/Page/AnnotationLayer.css";
 import "react-pdf/dist/esm/Page/TextLayer.css";
 pdfjs.GlobalWorkerOptions.workerSrc = `https://cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.js`;
 
-const resumeLink = `http://localhost:3000/static/media/Yashwanth%20M.d6964bbed3065ed00012.pdf`
+const resumeLink = `http://localhost:3001/ReactApp-Portfolio/static/media/Yashwanth%20M.d6964bbed3065ed00012.pdf`
 
 
 const Resume = () => {
